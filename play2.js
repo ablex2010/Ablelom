e018b7298c9082dbbd80cc0baae40eee18d8900a
Ablelom Abraha <ablex2010@gmail.com>
2020-11-30 03:10:51 -0600

    array coping

--- play2.js
+++ play2.js
@@ -1,12 +1,23 @@
-const hobbies = ['Sports', 'Cookies'];
+const person = {
+    name: 'Max',
+    age: 29,
+    greet(){
+        console.log('Hi I am ' + this.name);
+    }
+};
 
-console.log(hobbies);
-console.log(hobbies);
-console.log(hobbies);
-console.log(hobbies);
-console.log(hobbies);
-console.log(hobbies);
-console.log(hobbies);
-console.log(hobbies);
-console.log(hobbies);
-console.log(hobbies);
+console.log(person.name + person.age);
+
+const hobbies = ['Sports', 'Cookies'];
+// one way of getting all the elementes inside an array
+// for (let hobby of hobbies){
+//     console.log(hobby);
+// }
+
+// another way of rewriting 
+// console.log(hobbies.map(hobby => ' Hobby ' + hobby));
+//  console.log(hobbies);
+
+// another way of rewriting
+const copiedArray = hobbies.slice();
+console.log(copiedArray);
