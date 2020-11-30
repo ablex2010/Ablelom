07654a2f60356ea717e67700f32271595893feca
Ablelom Abraha <ablex2010@gmail.com>
2020-11-30 03:37:25 -0600

    distructoring

--- play2.js
+++ play2.js
@@ -5,37 +5,53 @@ const person = {
         console.log('Hi I am ' + this.name);
     }
 };
-// spread operator
-const copiedPerson = {...person};
-// console.log(person.name + person.age);
-console.log(copiedPerson);
 
-const hobbies = ['Sports', 'Cookies'];
-// one way of getting all the elementes inside an array
-// for (let hobby of hobbies){
-//     console.log(hobby);
-// }
-
-// another way of rewriting 
-// console.log(hobbies.map(hobby => ' Hobby ' + hobby));
-//  console.log(hobbies);
-
-// another way of rewriting but not quite the same output
-// const copiedArray = hobbies.slice();
-
-// spread operator
-const copiedArray = [...hobbies];
-console.log(copiedArray);
-
-// the listing operator
-
-// const toArray1 = (arg1, arg2, arg3) =>{
-//     return [arg1, arg2, arg3];
+// deconstruction this can be done by some third party 
+// not neccesasarly has to stick with the original one 
+// const printName = (personData) => {
+// console.log(personData.name);
 // };
-// console.log(toArray1(1, 2, 3));
 
-// or the rest operator
-const toArray = (...args) => {
-    return args;
-};
-console.log(toArray(1, 2, 3, 4));
+// printName(person);
+const printName = (personData) => {
+    console.log(personData.name);
+    };
+    
+    printName(person);
+
+
+// // spread operator
+// const copiedPerson = {...person};
+// // console.log(person.name + person.age);
+// console.log(copiedPerson);
+
+// const hobbies = ['Sports', 'Cookies'];
+// // one way of getting all the elementes inside an array
+// // for (let hobby of hobbies){
+// //     console.log(hobby);
+// // }
+
+// // another way of rewriting 
+// // console.log(hobbies.map(hobby => ' Hobby ' + hobby));
+// //  console.log(hobbies);
+
+// // another way of rewriting but not quite the same output
+// // const copiedArray = hobbies.slice();
+
+// // spread operator
+// const copiedArray = [...hobbies];
+// console.log(copiedArray);
+
+// // the listing operator but not flexible like the following one
+
+// // const toArray1 = (arg1, arg2, arg3) =>{
+// //     return [arg1, arg2, arg3];
+// // };
+// // console.log(toArray1(1, 2, 3));
+
+// // or the rest operator
+// const toArray = (...args) => {
+//     return args;
+// };
+// console.log(toArray(1, 2, 3, 4));
+
