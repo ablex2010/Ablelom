f04c66fb8ca768ad3bd89db5735ceea375d9031d
Ablelom Abraha <ablex2010@gmail.com>
2020-12-01 01:07:54 -0600

    request and response

--- Section-3/app.js
+++ Section-3/app.js
@@ -19,6 +19,12 @@ const http = require('http');
 const server = http.createServer((req, res)=>{
     console.log(req.url, req.method, req.headers);
     // process.exit();
+    res.setHeader('Content-TYpe', 'text/html');
+    res.write('<html>');
+    res.write('<header><title> My First Page</header></title>');
+    res.write('<body><h1>Hello world </body></h1>');
+    res.write('</html>');
+    res.end();
      });
      server.listen(3000);
 
